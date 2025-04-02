5b2ce6b82ec62bfa9e363cee15e941c2bbc4a930
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-04-02 18:15:59 -0300

    finish basic trikey.cpp

--- lafvin.cpp
+++ lafvin.cpp
@@ -29,5 +29,96 @@ void forwardFor(unsigned long ms){
 }
 
 void backward(){
-    
+    digitalWrite(2,LOW);
+    analogWrite(5,50);
+    digitalWrite(4,LOW);
+    analogWrite(6,50);
 }
+
+void backwardFor(unsigned long ms){
+    digitalWrite(2,LOW);
+    analogWrite(5,50);
+    digitalWrite(4,LOW);
+    analogWrite(6,50);
+
+    delay(ms);
+
+    analogWrite(5,0);
+    analogwrite(6,0);
+}
+
+void spinLeft(){
+    digitalWrite(2,HIGH);
+    analogWrite(5,50);
+    digitalWrite(4,LOW);
+    analogWrite(6,50);
+}
+
+void spinLeftFor(unsigned long ms){
+    digitalWrite(2,HIGH);
+    analogWrite(5,50);
+    digitalWrite(4,LOW);
+    analogWrite(6,50);
+
+    delay(ms);
+
+    analogWrite(5,0);
+    analogwrite(6,0);
+}
+
+void spinRight(){
+    digitalWrite(2,LOW);
+    analogWrite(5,50);
+    digitalWrite(4,HIGH);
+    analogWrite(6,50);
+}
+
+void spinRightFor(unsigned long ms){
+    digitalWrite(2,LOW);
+    analogWrite(5,50);
+    digitalWrite(4,HIGH);
+    analogWrite(6,50);
+
+    delay(ms);
+
+    analogWrite(5,0);
+    analogwrite(6,0);
+}
+
+void moveRight(){
+    analogWrite(5,0);
+    digitalWrite(4, HIGH);
+    analogWrite(6, _speed);
+}
+
+void moveRightFor(unsigned long ms){
+    analogWrite(5,0);
+    digitalWrite(4, HIGH);
+    analogWrite(6, _speed);
+
+    delay(ms);
+
+    analogwrite(6,0);
+}
+
+void moveLeft(){
+    analogWrite(6,0);
+    digitalWrite(2,HIGH);
+    analogWrite(5, _speed);
+}
+
+void moveLeftFor(unsigned long ms){
+    analogWrite(6,0);
+    digitalWrite(2,HIGH);
+    analogWrite(5, _speed);
+    
+    delay(ms);
+
+    analogwrite(5,0);
+}
+
+void stop(){
+    analogWrite(5,0);
+    analogwrite(6,0);
+}
+
