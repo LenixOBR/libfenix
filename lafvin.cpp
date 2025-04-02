e9886ed00f3bf3546997e39b852227192f9eec07
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-04-02 18:40:45 -0300

    final fix

--- lafvin.cpp
+++ lafvin.cpp
@@ -1,142 +1,119 @@
-#include "Arduino.h"
 #include "lafvin.h"
+#include <stdlib.h> // Necessário para a função abs()
 
-Lafvin::Lafvin(int speed){
+Lafvin::Lafvin(int speed) {
+    _speed = speed;
+    pinMode(2, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    Serial.begin(9600);
+}
+
+void Lafvin::setSpeed(int speed) {
     _speed = speed;
 }
 
-void setSpeed(int speed){
-    _speed = speed;
-}
-
-void forward(){
-    digitalWrite(2,HIGH);
-    analogWrite(5,_speed);
-    digitalWrite(4,HIGH);
-    analogWrite(6,_speed);
-}
-
-void forwardFor(unsigned long ms){
-    digitalWrite(2,HIGH);
-    analogWrite(5,_speed);
-    digitalWrite(4,HIGH);
-    analogWrite(6,_speed);
-
-    delay(ms);
-
-    analogWrite(5,0);
-    analogwrite(6,0);
-}
-
-void backward(){
-    digitalWrite(2,LOW);
-    analogWrite(5,_speed);
-    digitalWrite(4,LOW);
-    analogWrite(6,_speed);
-}
-
-void backwardFor(unsigned long ms){
-    digitalWrite(2,LOW);
-    analogWrite(5,_speed);
-    digitalWrite(4,LOW);
-    analogWrite(6,_speed);
-
-    delay(ms);
-
-    analogWrite(5,0);
-    analogwrite(6,0);
-}
-
-void spinLeft(){
-    digitalWrite(2,HIGH);
-    analogWrite(5,_speed);
-    digitalWrite(4,LOW);
-    analogWrite(6,_speed);
-}
-
-void spinLeftFor(unsigned long ms){
-    digitalWrite(2,HIGH);
-    analogWrite(5,_speed);
-    digitalWrite(4,LOW);
-    analogWrite(6,_speed);
-
-    delay(ms);
-
-    analogWrite(5,0);
-    analogwrite(6,0);
-}
-
-void spinRight(){
-    digitalWrite(2,LOW);
-    analogWrite(5,_speed);
-    digitalWrite(4,HIGH);
-    analogWrite(6,_speed);
-}
-
-void spinRightFor(unsigned long ms){
-    digitalWrite(2,LOW);
-    analogWrite(5,_speed);
-    digitalWrite(4,HIGH);
-    analogWrite(6,_speed);
-
-    delay(ms);
-
-    analogWrite(5,0);
-    analogwrite(6,0);
-}
-
-void moveRight(){
-    analogWrite(5,0);
+void Lafvin::forward() {
+    // Movimento para frente: ambos os motores com direção forward
+    digitalWrite(2, HIGH);
     digitalWrite(4, HIGH);
+    analogWrite(5, _speed);
     analogWrite(6, _speed);
 }
 
-void moveRightFor(unsigned long ms){
-    analogWrite(5,0);
-    digitalWrite(4, HIGH);
+void Lafvin::forwardFor(unsigned long ms) {
+    forward();
+    delay(ms);
+    stop();
+}
+
+void Lafvin::backward() {
+    // Movimento para trás: ambos os motores com direção backward
+    digitalWrite(2, LOW);
+    digitalWrite(4, LOW);
+    analogWrite(5, _speed);
     analogWrite(6, _speed);
+}
 
+void Lafvin::backwardFor(unsigned long ms) {
+    backward();
     delay(ms);
-
-    analogwrite(6,0);
+    stop();
 }
 
-void moveLeft(){
-    analogWrite(6,0);
-    digitalWrite(2,HIGH);
+void Lafvin::spinLeft() {
+    // Gira para a esquerda: motor esquerdo forward e direito backward
+    digitalWrite(2, HIGH);
+    digitalWrite(4, LOW);
     analogWrite(5, _speed);
+    analogWrite(6, _speed);
 }
 
-void moveLeftFor(unsigned long ms){
-    analogWrite(6,0);
-    digitalWrite(2,HIGH);
-    analogWrite(5, _speed);
-    
+void Lafvin::spinLeftFor(unsigned long ms) {
+    spinLeft();
     delay(ms);
-
-    analogwrite(5,0);
+    stop();
 }
 
-void stop(){
-    analogWrite(5,0);
-    analogwrite(6,0);
+void Lafvin::spinRight() {
+    // Gira para a direita: motor esquerdo backward e direito forward
+    digitalWrite(2, LOW);
+    digitalWrite(4, HIGH);
+    analogWrite(5, _speed);
+    analogWrite(6, _speed);
 }
 
-void preciseMove(int L, int R){
-    digitalWrite(2, L > 0) ? HIGH : LOW);
+void Lafvin::spinRightFor(unsigned long ms) {
+    spinRight();
+    delay(ms);
+    stop();
+}
+
+void Lafvin::moveLeft() {
+    // Curva para a esquerda: reduz a velocidade do motor esquerdo
+    digitalWrite(2, HIGH);
+    digitalWrite(4, HIGH);
+    analogWrite(5, 0);  // Motor esquerdo com metade da velocidade
+    analogWrite(6, _speed);
+}
+
+void Lafvin::moveLeftFor(unsigned long ms) {
+    moveLeft();
+    delay(ms);
+    stop();
+}
+
+void Lafvin::moveRight() {
+    // Curva para a direita: reduz a velocidade do motor direito
+    digitalWrite(2, HIGH);
+    digitalWrite(4, HIGH);
+    analogWrite(5, _speed);
+    analogWrite(6, 0);  // Motor direito com metade da velocidade
+}
+
+void Lafvin::moveRightFor(unsigned long ms) {
+    moveRight();
+    delay(ms);
+    stop();
+}
+
+void Lafvin::stop() {
+    // Para ambos os motores
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+}
+
+void Lafvin::preciseMove(int L, int R) {
+    // Controle preciso: define a direção com base no sinal e a velocidade com o valor absoluto
+    digitalWrite(2, (L >= 0) ? HIGH : LOW);
+    digitalWrite(4, (R >= 0) ? HIGH : LOW);
     analogWrite(5, abs(L));
-    digitalWrite(4, R > 0) ? HIGH : LOW);
     analogWrite(6, abs(R));
 }
 
-void preciseMoveFor(int L, int R, unsigned long ms){
-    digitalWrite(2, L > 0) ? HIGH : LOW);
-    analogWrite(5, abs(L));
-    digitalWrite(4, R > 0) ? HIGH : LOW);
-    analogWrite(6, abs(R));
-
+void Lafvin::preciseMoveFor(int L, int R, unsigned long ms) {
+    preciseMove(L, R);
     delay(ms);
-
-    analogWrite(5,0);
-    analogwrite(6,0);
+    stop();
 }
