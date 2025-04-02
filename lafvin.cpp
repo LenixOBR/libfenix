bc7eb52cba8bc2a6d4fd9c97dd814ffec8c29e7e
Mircas001 <79532341+Mircas001@users.noreply.github.com>
2025-04-02 18:05:41 -0300

    add lafvin.h

--- lafvin.cpp
+++ lafvin.cpp
@@ -1,1 +1,33 @@
+#include "Arduino.h"
+#include "lafvin.h"
 
+Lafvin::Lafvin(int speed){
+    _speed = speed;
+}
+
+void setSpeed(int speed){
+    _speed = speed;
+}
+
+void forward(){
+    digitalWrite(2,HIGH);
+    analogWrite(5,_speed);
+    digitalWrite(4,HIGH);
+    analogWrite(6,_speed);
+}
+
+void forwardFor(unsigned long ms){
+    digitalWrite(2,HIGH);
+    analogWrite(5,_speed);
+    digitalWrite(4,HIGH);
+    analogWrite(6,_speed);
+
+    delay(ms);
+
+    analogWrite(5,0);
+    analogwrite(6,0);
+}
+
+void backward(){
+    
+}
--- lafvin.h
+++ lafvin.h
@@ -1,1 +1,46 @@
+/*
+  lafvin.h - Library for moving lafvin robots.
+  Created by Lenix Team, March 13, 2025
+  Released into the public domain.
+*/
+
+#ifndef lafvin_h
+#define lafvin_h
+
+/*
+    VS Code is complaining about arduino.h, let's hope it's nothing.
+    https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide/ <- guide being used
+*/
+
+#include "Arduino.h"
+#include "AFMotor.h"
+
+/*L=Left, R=Right*/
+
+class Lafvin
+{
+  public:
+    Lafvin(int speed);
+    void setSpeed(int speed);
+    void forward();
+    void forwardFor(unsigned long ms);
+    void backward();
+    void backwardFor(unsigned long ms);
+    void spinLeft();
+    void spinLeftFor(unsigned long ms);
+    void spinRight();
+    void spinRightFor(unsigned long ms);
+    void moveLeftFor(unsigned long ms);
+    void moveLeft();
+    void moveRightFor(unsigned long ms);
+    void moveRight();
+    void stop();
+    void preciseMove(int L, int R);
+    void preciseMoveFor(int L, int R, int ms);
+  private:
+    int _speed;
+
+};
+
+#endif
 
